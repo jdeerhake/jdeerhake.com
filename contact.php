dcb35ff652c410bc51101305f236dbf6bf020b88
John Deerhake <jdeerhake@gmail.com>
2011-01-28 21:25:23 -0800

    reorganize contact me

--- contact.php
+++ contact.php
@@ -6,19 +6,17 @@
 	<dl class="contactMe">
 		<dt>Email</dt>
 			<dd><a href="mailto:jdeerhake@gmail.com">jdeerhake@gmail.com</a></dd>
-		<dt>Address</dt>
-			<dd>33 S Wilson Ave Apt 205<br />Pasadena, CA 91106</dd>
 	</dl>
 </div>
 
 <div class="floatLeft">
 	<h2>Links</h2>
 	<div class="socialGrid">
+		<a href="http://twitter.com/jdeerhake"><img class="socialIcon" src="images/twitter.png" alt="Twitter" /></a>
 		<a href="http://github.com/jdeerhake"><img class="socialIcon" src="images/github.png" alt="Github" /></a>
 		<a href="http://facebook.com/deerhake11"><img class="socialIcon" src="images/facebook.png" alt="Facebook" /></a>
 		<a href="http://flickr.com/photos/deerhake11"><img class="socialIcon" src="images/flickr.png" alt="Flickr" /></a>
-		<a href="http://twitter.com/jdeerhake"><img class="socialIcon" src="images/twitter.png" alt="Twitter" /></a>
-		<a href="http://forrst.com/people/jdeerhake"><img class="socialIcon" src="images/forrst.png" alt="Forrst" /></a>
+		<a href="http://forrst.me/jdeerhake"><img class="socialIcon" src="images/forrst.png" alt="Forrst" /></a>
 		<a href="http://beeradvocate.com/user/profile/jdeerhake"><img class="socialIcon" src="images/beer.png" alt="BeerAdvocate" /></a>
 	</div>
 </div>
