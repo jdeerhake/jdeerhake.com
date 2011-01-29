4253e988c5af94fbee0c9f8a7e23afeccf71ce3e
John Deerhake <jdeerhake@gmail.com>
2011-01-28 20:22:52 -0800

    Add cachedAjax to code

--- code.php
+++ code.php
@@ -4,6 +4,7 @@
 
 <dl class="projectList">
 	<dt>jQuery Plugins</dt>
+  <dd><a href="http://blog.jdeerhake.com/cachedajax-jquery-plugin">cachedAjax</a> - Memoized Ajax functionality</dd>
 	<dd><a href="http://blog.jdeerhake.com/bgfader-jquery-plugin">bgFader</a> - Allows you to fade in/out a background image of an element independent of its content.</dd>
 	<dd><a href="http://blog.jdeerhake.com/inputhintoverlay-jquery-plugin">inputHintOverlay</a> - Adds label overlays to form input fields</dd>
 </dl>
