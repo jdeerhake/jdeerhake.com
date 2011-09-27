e9a63308b2ef38223988cf335df916b09071c14d
John Deerhake <jdeerhake@gmail.com>
2011-09-27 16:38:46 -0700

    Remove my stuff

--- about.php
+++ about.php
@@ -6,45 +6,21 @@
 	<a><img class="right flickr-random" /></a>
 </div>
 <h2>About Me</h2>
-<p class="narrow">I grew up outside the small town of LaFayette, Ohio (<a href="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lafayette,+OH&amp;sll=37.0625,-95.677068&amp;sspn=84.512694,185.449219&amp;ie=UTF8&amp;hq=&amp;hnear=Lafayette,+Allen,+Ohio&amp;z=15&amp;iwloc=A">where?</a>).  
-	After graduating from high school I moved to Columbus and attended Ohio State.  While taking classes I worked multiple part time jobs simultaneously including working as an 
-	<a href="http://www.homecityice.com">ice delivery man</a>, <a href="http://www.uaschools.org/index.aspx?nid=155">jr. high track coach</a>, and as an 
+<p class="narrow">I grew up outside the small town of LaFayette, Ohio (<a href="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lafayette,+OH&amp;sll=37.0625,-95.677068&amp;sspn=84.512694,185.449219&amp;ie=UTF8&amp;hq=&amp;hnear=Lafayette,+Allen,+Ohio&amp;z=15&amp;iwloc=A">where?</a>).
+	After graduating from high school I moved to Columbus and attended Ohio State.  While taking classes I worked multiple part time jobs simultaneously including working as an
+	<a href="http://www.homecityice.com">ice delivery man</a>, <a href="http://www.uaschools.org/index.aspx?nid=155">jr. high track coach</a>, and as an
 	IT professional.</p>
 
-<p class="narrow">I like to travel as much as I can, I've driven through and visited about 35 US states and 9 countries in Europe. Since growing up in Ohio, I've lived for short periods of time in Glendale, California; 
+<p class="narrow">I like to travel as much as I can, I've driven through and visited about 35 US states and 9 countries in Europe. Since growing up in Ohio, I've lived for short periods of time in Glendale, California;
 	Darmstadt-Wixhausen, Germany; Hanover, New Hampshire; Boston, Massachusetts; and Pasadena, California.</p>
 
-<p class="narrow">I got my first computer in 1996 - a Gateway2000 with a Pentium Pro 180 MHz. I instanly began playing around with it and almost just as quickly broke it.  After I repeated this process once 
-	or twice I decided to teach myself to fix it.  I've been fixing computers (and breaking them) ever since.  I've also taken to building and upgrading them as a hobby.</p> 
+<p class="narrow">I got my first computer in 1996 - a Gateway2000 with a Pentium Pro 180 MHz. I instanly began playing around with it and almost just as quickly broke it.  After I repeated this process once
+	or twice I decided to teach myself to fix it.  I've been fixing computers (and breaking them) ever since.  I've also taken to building and upgrading them as a hobby.</p>
 
-<p class="narrow">In the midst of that experience I was introduced to coding by my cousin, Dustin.  I started with QBasic in 1996 and that led me to C and C++. Around that same year, I made my first 
+<p class="narrow">In the midst of that experience I was introduced to coding by my cousin, Dustin.  I started with QBasic in 1996 and that led me to C and C++. Around that same year, I made my first
 	webpage using HTML and instantly loved web programming. </p>
-
-<p class="narrow">&nbsp;</p>
-<h2>Stuff I Use</h2>
-
-<p class="narrow">My favorite way to develop is using:</p>
-
-<dl class="myStuff">
-	<dt>Linux<dt>
-		<dd>More specifically, <a href="http://linuxmint.com">Linux Mint</a> 8 (Helena).  <a href="http://en.wikipedia.org/wiki/Free_and_open_source_software">FOSS</a>.</dd>
-	<dt>gedit</dt>
-		<dd>GUI text editor. FOSS.</dd>
-	<dt>Firefox with Firebug</dt>
-		<dd>My development browser is <a href="http://getfirefox.com">Firefox</a> 3.6 with the <a href="http://getfirebug.com/">Firebug</a> developer's extension. FOSS.<dd>
-	<dt>Google Chrome</dt>
-		<dd>My general purpose <a href="http://www.google.com/chrome">browser</a>. FOSS.</dd>
-	<dt>LAMP<dt>
-		<dd>Linux, <a href="http://www.apache.org/">Apache</a> web server, <a href="http://www.mysql.com/">MySQL</a> database, <a href="http://php.net/">PHP</a> server side coding. All FOSS.</dd>
-	<dt>jQuery<dt>
-		<dd><a href="http://jquery.com">jQuery</a> Javascript framework. FOSS.</dd>
-	<dt>GIMP</dt>
-		<dd><a href="http://gimp.org">GIMP</a> image manipulation program. FOSS.</dd>
-	<dt>Lenovo R61</dt>
-		<dd>All of my work is done on my trusty <a href="http://lenovo.com">Lenovo</a> R61 notebook.</dd>
-	
-</dl>
 
 
 <div class="clearFloat"></div>
 <?php require_once('include/footer.php'); ?>
+
