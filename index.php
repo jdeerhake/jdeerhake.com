5446ca908b94b9701cd16a11a41f115761f4ca2d
John Deerhake <jdeerhake@gmail.com>
2011-09-27 23:31:33 -0700

    Add promote JS

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 
 <h2>Hi There!</h2>
 
-<p>My name is John Deerhake and I am a <?php echo myage(); ?> year old front-end web developer currently living in Pasadena, California and working for <a href="http://www.goldstar.com">Goldstar.com</a>. I love writing Javascript and making the web faster.  I also enjoy writing some CSS and HTML as well.</p>
+<p>My name is John Deerhake and I am a <?php echo myage(); ?> year old front-end web developer currently living in Pasadena, California and working for <a href="http://www.goldstar.com">Goldstar.com</a>. I love writing Javascript and making the web faster.  I also enjoy writing some CSS and HTML fromt time to time.</p>
 <p>In my free time I like to go camping, hiking, ride my bike, cook, and throw discus. The pictures on this site are random pulls from my Flickr stream that represent a lot of the things I enjoying doing while not working.</p>
 
+<p><a href='https://developer.mozilla.org/en/JavaScript' title='JavaScript Reference'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript Reference'/></a></p>
 <?php require_once('include/footer.php'); ?>
 
