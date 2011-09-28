4602c7f9dd1f771c98a42883e62ca54b6375dcce
John Deerhake <jdeerhake@gmail.com>
2011-09-27 23:28:04 -0700

    Text update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <h2>Hi There!</h2>
 
-<p>My name is John Deerhake and I am a <?php echo myage(); ?> year old front-end web developer currently living in Pasadena, California and working for <a href="http://www.goldstar.com">Goldstar.com</a>. I love writing good, reusable ECMAScript (erm.. Javascript) and I enjoy some CSS and (X)HTML as well.</p>
+<p>My name is John Deerhake and I am a <?php echo myage(); ?> year old front-end web developer currently living in Pasadena, California and working for <a href="http://www.goldstar.com">Goldstar.com</a>. I love writing Javascript and making the web faster.  I also enjoy writing some CSS and HTML as well.</p>
 <p>In my free time I like to go camping, hiking, ride my bike, cook, and throw discus. The pictures on this site are random pulls from my Flickr stream that represent a lot of the things I enjoying doing while not working.</p>
 
 <?php require_once('include/footer.php'); ?>
