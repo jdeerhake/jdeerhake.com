9ebd8b1bdcd1cc8794035afb307b3282a127a8c9
John Deerhake <jdeerhake@gmail.com>
2018-06-04 10:55:39 -0700

    Remove MDN banner

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 <p>My name is John Deerhake and I am a front end web developer currently living in Santa Monica and working for <a href="https://travo.com">TRAVO</a>. I love building products and using modern development technologies like React, Angular, and React Native.</p>
 <p>In my free time I like to travel, go camping & hiking, and am an aspiring triathlete. The pictures on this site are random pulls from my Flickr stream that represent a lot of the things I enjoying doing while not sitting at a computer.</p>
 
-<p><a href='https://developer.mozilla.org/en/JavaScript' title='JavaScript Reference'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript Reference'/></a></p>
-
 <?php $images = Array(
   'http://jdeerhake.com' => 'https://c2.staticflickr.com/8/7484/27612009465_15f787f574_z.jpg'
 ); ?>
