fa57a6fd62442cd91f76c34bc89a5a1df6468b36
John Deerhake <jdeerhake@gmail.com>
2018-06-04 10:55:03 -0700

    Copy change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <h2>Hi There!</h2>
 
-<p>My name is John Deerhake and I am a front end web developer currently living in Santa Monica and working for <a href="https://travo.com">TRAVO</a>. I love writing Javascript and making the web faster.  I also enjoy writing some CSS and HTML fromt time to time.</p>
+<p>My name is John Deerhake and I am a front end web developer currently living in Santa Monica and working for <a href="https://travo.com">TRAVO</a>. I love building products and using modern development technologies like React, Angular, and React Native.</p>
 <p>In my free time I like to travel, go camping & hiking, and am an aspiring triathlete. The pictures on this site are random pulls from my Flickr stream that represent a lot of the things I enjoying doing while not sitting at a computer.</p>
 
 <p><a href='https://developer.mozilla.org/en/JavaScript' title='JavaScript Reference'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript Reference'/></a></p>
